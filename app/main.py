788c206b7f176bd760206166992d817569b1182f
Jimmy <jimbotela@gmail.com>
2022-12-25 05:38:46 +0300

    modified main.py

--- app/main.py
+++ app/main.py
@@ -1,17 +1,20 @@
 from flask import Flask, request, Response 
 from prometheus_client import Counter, generate_latest
 
+
 REQUEST_COUNT = Counter("app_request_counter", "Total Number of Requests made on Application")
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
   REQUEST_COUNT.inc()
-  return "Hello World, This is from Docker WSGI"
+  return "Hello World, This is from Docker WSGI" 
 
 @app.route('/metrics')
 def metrics():
+  REQUEST_COUNT.inc()
   return Response(generate_latest(), 200, mimetype='plain/text')
 
 if __name__ == '__main__':
